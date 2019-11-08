397c73dbfb52bcaa3faac936f758e687f0c37de6
WestleyK <westley@sylabs.io>
2019-11-07 18:15:32 -0800

    Impoved handing if dup paths

--- src/main-whereis.c
+++ src/main-whereis.c
@@ -1,8 +1,8 @@
 // created by: WestleyR
 // email: westleyr@nym.hush.com
 // https://github.com/WestleyR/whereis
-// date: Nov 5, 2019
-// version-1.0.0
+// date: Nov 9, 2019
+// version-1.0.1
 //
 // The Clear BSD License
 //
@@ -18,46 +18,51 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <getopt.h>
 
-int main(int argc, char** argv) {
+#define SCRIPT_VERSION "v1.0.1, Nov 9, 2019"
 
-  if (argc != 2) {
-    printf("Usage: %s <command>\n", argv[0]);
-    return(1);
-  }
+void print_version() {
+  printf("%s\n", SCRIPT_VERSION);
+}
 
-  char *path_str;
-  path_str = (char*) malloc(5000);
-  strcpy(path_str, getenv("PATH"));
+void print_usage(const char* script_name) {
+  printf("Usage: %s [option] <command>\n", script_name);
+  printf("\n");
+  printf("Options:\n");
+  printf("  --path-only, -m  only print the command path\n");
+  printf("  --help, -h       print this help menu\n");
+  printf("  --version, -V    print the version for %s\n", script_name);
+  printf("\n");
+  printf("Source code: https://github.com/WestleyR/whereis\n");
+}
 
+int find_command(char **path, int path_len, const char* prog_name, int path_only) {
   struct stat info;
   int found = 1;
-
   struct dirent *dir;
   DIR *d;
 
-  char* prog_name = argv[1];
-
-  while (path_str[0]) {
-    size_t i = 0;
-    for (; path_str[i] && path_str[i] != ':'; ++i);
-    path_str[i] = '\0';
-    d = opendir(path_str);
+  for (int i = 0; i < path_len; i++) {
+    d = opendir(path[i]);
     if (d != NULL) {
       while ((dir = readdir(d)) != NULL) {
         if (strcmp(dir->d_name, prog_name) == 0) {
           char full_file_path[256];
           full_file_path[0] = '\0';
-
-          strcat(full_file_path, path_str);
+          strcat(full_file_path, path[i]);
           strcat(full_file_path, "/");
           strcat(full_file_path, prog_name);
-          printf("%s", full_file_path);
+          if (path_only != 0) {
+            printf("%s: %s", prog_name, full_file_path);
+          } else {
+            printf("%s", full_file_path);
+          }
           if (lstat(full_file_path, &info) != 0) {
             perror("lstat");
             return(1);
           }
-          if (S_ISLNK(info.st_mode)) {
+          if (S_ISLNK(info.st_mode) && path_only != 0) {
             char symlink_path[126];
             ssize_t len = readlink(full_file_path, symlink_path, sizeof(symlink_path));
             if (len != -1) {
@@ -72,16 +77,95 @@ int main(int argc, char** argv) {
         }
       }
       closedir(d);
+      if (path_only == 0 && found == 0) break;
     }
-    path_str += i + 1;
   }
 
-  if (found != 0) {
+  return(found);
+}
+
+int main(int argc, char** argv) {
+  int opt = 0;
+  int path_only = 1;
+
+  static struct option long_options[] = {
+    {"path-only", no_argument, 0, 'm'},
+    {"help", no_argument, 0, 'h'},
+    {"version", no_argument, 0, 'V'},
+    {NULL, 0, 0, 0}
+  };
+
+  while ((opt = getopt_long(argc, argv, "mhV", long_options, 0)) != -1) {
+    switch (opt) {
+      case 'm':
+        path_only = 0;
+        break;
+
+      case 'V':
+        print_version();
+        return(0);
+
+      case 'h':
+        print_usage(argv[0]);
+        return(0);
+
+      default:
+        return(22);
+    }
+  }
+
+  char* pathenv = getenv("PATH");
+  char spliter[] = ":";
+
+  int p = 0;
+  char** path;
+  path = (char **) malloc(50);
+  path[0] = (char*) malloc(50);
+  if (path == NULL) {
+    printf("malloc failed!\n");
+    perror("malloc");
     return(1);
   }
+
+  char *pathname = strtok(pathenv, spliter);
+  while (pathname != NULL) {
+    if (p == 0) {
+      strcpy(path[p], pathname);
+      p++;
+    }
+
+    int match = 0;
+    for (int i = 0; i < p; i++) {
+      if (strcmp(path[i], pathname) == 0) {
+        match = 1;
+      }
+    }
+    if (match != 1) {
+      path[p] = (char*) malloc(50);
+      strcpy(path[p], pathname);
+      p++;
+    }
+
+    pathname = strtok(NULL, spliter);
+  }
+
+  if (optind < argc) {
+    if (argc - optind > 1) {
+      print_usage(argv[0]);
+      return(22);
+    }
+    int found = find_command(path, p, argv[optind], path_only);
+    if (found != 0) {
+      return(1);
+    }
+  } else {
+    print_usage(argv[0]);
+    return(22);
+  }
+
+  free(path);
 
   return(0);
 }
 
-
-// vim: tabstop=2 shiftwidth=2 expandtab
+// vim: tabstop=2 shiftwidth=2 expandtab autoindent softtabstop=0
